66855f16d4ea4fa71c022e6c91a237c549ef51d8
raymondtju <raymond.rtju@gmail.com>
2023-02-28 13:11:37 +0700

    fix routing and add not found middleware

--- app.js
+++ app.js
@@ -6,35 +6,37 @@ const logger = require("morgan");
 const app = express();
 const users = require("./app/api/v1/users/router");
 const handlerError = require("./app/middleware/handler-error");
+const notFound = require("./app/middleware/not-found");
 
-app.use(logger("dev"));
+app.use(logger("common"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser());
 
-app.use("/api/v1/users", users);
-app.use("/", (req, res) => {
+app.get("/", (req, res) => {
   res.status(200).json({
     message: "LessCode API",
   });
 });
+app.use("/api/v1/users", users);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+// // catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
 
+app.use(notFound);
 app.use(handlerError);
 
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render("error");
+// });
 
 module.exports = app;
